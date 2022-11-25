2df51f437c4f8c1200579770373a3fc738a731fe
hikemalliday <hikemalliday@gmail.com>
2022-11-25 13:29:56 -0600

    pushArrayToStack, etc

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+///FIX POINTERS
+//Make method that prints stack
+//Pop until you drop at 'x'
+//Method that takes in an array of numbers
+//However many numbers that is, it pushes them all in the array order
+
+
 class Node 
 {
     constructor(data, pointer = null)
@@ -11,7 +19,6 @@ class Stack
 {
     constructor()
     {
-       
         this.pointer;
         this.top;
         this.count = 0;
@@ -50,19 +57,54 @@ class Stack
         //Retract the count
         this.count--;
     }
-};
+
+    popDataUntil(stackNumber)
+    {
+        
+        while (stackNumber !== this.count)
+        {
+            let current = this.top;
+            
+            //remove the top nodes pointer:
+            current.pointer = null;
+        
+            //change top to next node down the stack:
+            this.top = this.pointer;
+            
+            //Change global pointer
+            this.pointer = this.top.pointer;
+            
+            //Retract the count
+            this.count--;
+        }
+
+    }
+
+    pushArrayToStack(array)
+    {
+        for (let i = 0; i < array.length; i++)
+        {
+            let node = new Node(array[i]);
+            this.pushData(node);
+        }
+    }
+
+}
+
+
 
 let stack = new Stack();
 let node1 = new Node("Hi there, i'm Node1!");
 let node2 = new Node("Hi there, i'm Node2!");
 let node3 = new Node("Hi there, i'm Node3!");
 let node4 = new Node("Hi there, i'm Node4!");
+let node5 = new Node("Hi there, i'm Node5!");
+let node6 = new Node("Hi there, i'm Node6!");
+let node7 = new Node("Hi there, i'm Node7!");
 
-stack.pushData(node1);
-stack.pushData(node2);
-stack.pushData(node3);
-stack.pushData(node4);
-stack.popData();
+let array1 = ['This', 'Is', 'Just', 'A', 'Test'];
+
+stack.pushArrayToStack(array1);
 
 
 
